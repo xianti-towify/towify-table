564b5efedbaac8299217769b9eca8acf90097390
xianti-towify <xianti.xiong@towify.com>
2021-07-02 14:46:37 +0800

    fix(towify-table): 代码优化

--- projects/towify-table/src/projects.ts
+++ projects/towify-table/src/projects.ts
@@ -3,5 +3,7 @@
  */
 
 export * from './lib/type/towify.table.type';
+export * from './lib/driectives/towify.row.def.directive';
+export * from './lib/driectives/towify.cell.def.directive';
 export * from './lib/towify-table/towify-table.component';
 export * from './lib/towify.table.module';
